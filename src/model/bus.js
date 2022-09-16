0af69180029b225e63f0a579fc3a82e4e57b30b4
brguamanc <brguamanc@utn.edu.ec>
2022-09-16 17:04:15 -0500

    correcciones

--- src/model/bus.js
+++ src/model/bus.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const busSchema = mongoose.Schema({
+  viaje_id: {
+    type: String,
+    required: true,
+  },
   empresa: {
     type: String,
     required: true,
@@ -21,10 +25,10 @@ const busSchema = mongoose.Schema({
     type: String,
     required: true,
   },
-  updated: { 
-    type: Date, 
-    default: Date.now 
-},
+  updated: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("Bus", busSchema);
--- src/routes/busRoutes.js
+++ src/routes/busRoutes.js
@@ -4,19 +4,56 @@ const busSchema=require('../model/bus')
 
 // create city
 
-router.get("/bus", (req, res) => {
-   busSchema
-     .find()
-     .then((data) => res.json(data))
-     .catch((error) => res.json({ message: error }));
-});
 
-router.post('/buses',(req,res)=>{
+router.post('/bus',(req,res)=>{
     const bus = busSchema(req.body);
     bus
     .save()
     .then((data)=>res.json(data))
     .catch((error)=>res.json({message:error}))
 })
+
+
+router.get("/buses", (req, res) => {
+  busSchema
+    .find()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+router.get("/buses", (req, res) => {
+  busSchema
+    .find()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+router.get("/bus/:id", (req, res) => {
+    const { id } = req.params;
+    busSchema
+      .findById(id)
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+});
+
+router.put("/bus/:id", (req, res) => {
+  const { id } = req.params;
+  const {viaje_id,empresa,destino,origen,horaSalida,horaLlegada}=req.body;
+  busSchema
+    .updateOne(
+      { _id: id },
+      {$set: {viaje_id, empresa, destino, origen, horaSalida, horaLlegada} }
+    )
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+router.delete("/bus/:id", (req, res) => {
+  const { id } = req.params;
+  busSchema
+    .remove({_id:id})
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
 
 module.exports=router
