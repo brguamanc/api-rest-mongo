4ccd9e0c2d8c8df4e45501d8cf1d514a3772009f
brguamanc <byrong1998@gmail.com>
2022-12-13 21:18:32 -0500

    prueba

--- src/routes/login.js
+++ src/routes/login.js
@@ -14,7 +14,7 @@ router.post("/usuario", (req, res) => {
         err: erro,
       });
     }
-    // Verifica que exista un usuario con el mail escrita por el usuario.
+    // Verifica que exista un usuario con el mail escrita por el usuario new.
     if (!usuarioDB) {
       return res.status(400).json({
         ok: false,
